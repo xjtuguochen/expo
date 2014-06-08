2551e9e316cc99662650afe369a339f6717e7228
albert <xjtuguochen@gmail.com>
2014-06-08 23:51:41 +0800

    check point

--- src/test/java/com/terracotta/AppTest.java
+++ src/test/java/com/terracotta/AppTest.java
@@ -1,6 +1,11 @@
 package com.terracotta;
 
+import static org.hamcrest.Matchers.*;
+
 import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
 
 /**
  * Created by johndoe on 6/8/14.
@@ -8,6 +13,6 @@ import org.junit.Test;
 public class AppTest {
     @Test
     public void testEqual() {
-
+        assertThat(1, equalTo(1));
     }
 }
